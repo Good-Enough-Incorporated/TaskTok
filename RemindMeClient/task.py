a145767cf51a2577bae3b5fe7db12d361f29b2cc
beatles1235 <beatles1235@gmail.com>
2023-11-13 16:27:51 -0500

    missing input for function

--- RemindMeClient/task.py
+++ RemindMeClient/task.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_mail import Mail, Message
 
 @shared_task(bind=True)
-def send_email(email_to, subject, body):
+def send_email(self, email_to, subject, body):
     #args subject, recipient, body, creationDate
     #from TaskTok.models import taskReminder
     #print(f"[{creationDate}]: Pretending to send an email to *{recipient}* subject= {subject} body = {body}")
