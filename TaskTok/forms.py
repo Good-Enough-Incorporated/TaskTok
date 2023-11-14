ab6f53e10b365291983fd43e04a5f496ff76d9d8
jfergesonumgc <150169966+jfergesonumgc@users.noreply.github.com>
2023-11-14 16:22:06 -0500

    Added email and password validation to register form
    Added checks for email format and password requirements to forms.py.

--- TaskTok/forms.py
+++ TaskTok/forms.py
@@ -4,9 +4,32 @@ for all web forms
 """
 import re
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField, ValidationError)
-from wtforms.validators import DataRequired, Email, InputRequired, Length
+from wtforms import (StringField, PasswordField, SubmitField, ValidationError, EmailField)
+from wtforms.validators import DataRequired, InputRequired, Length
 from wtforms.widgets import (SubmitInput)
+
+def validate_email(form, field):
+    # Found this regex here:
+    # https://emailregex.com/
+    if re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', field.data):
+        return
+    raise ValidationError('Email must match the standard format: user@domain.tld')
+
+def validate_password(form, field):
+    # NIST password recommendations:
+    # https://www.auditboard.com/blog/nist-password-guidelines/
+    # https://github.com/usnistgov/800-63-3
+    min_length = 12
+    max_length = 64
+    if len(field.data) < min_length:
+        raise ValidationError(f'Password must be at least {min_length} characters')
+    if len(field.data) > max_length:
+        raise ValidationError(f'Password cannot be more than {max_length} characters')
+    if re.match('^[A-Za-z0-9._@$!%*#?&^-]*$', field.data):
+        return #String contains valid characters only
+    raise ValidationError('Password may only contain letters, numbers,'
+                         +' and the following characters: . _ @ $ ! % * # ? & ^ -')
+
 def validate_username(form, field):
     #if len(field.data) == 0:
     #    raise ValidationError('Username is required')
@@ -21,10 +44,10 @@ def validate_username(form, field):
     if re.match('^[0-9]*$', field.data):
         raise ValidationError('Username must contain letters')
     if re.match('^[A-Za-z0-9]*$', field.data):
-        return # String contains valid characters only
+        return #String contains valid characters only
     raise ValidationError('Username may only contain letters and numbers')
     
 class NewUserForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), validate_email])
     username = StringField('Username', validators=[InputRequired(), validate_username])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[InputRequired(), validate_password])
