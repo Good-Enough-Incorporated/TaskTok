418078dd7e68cd4f3f111ebcf46f2ec94935bfaf
John Aciares <49409536+jaciares@users.noreply.github.com>
2023-11-24 18:02:30 -0500

    cleaned up celeryManager.py and added more pep8 to requirements.txt

--- RemindMeClient/celeryManager.py
+++ RemindMeClient/celeryManager.py
@@ -3,15 +3,17 @@ from flask import Flask
 import functools
 from threading import RLock
 import kombu.utils
-#bug with python 3.12 and celery
-#https://github.com/celery/kombu/issues/1804
+
+# bug with python 3.12 and celery
+# https://github.com/celery/kombu/issues/1804
 if not getattr(kombu.utils.cached_property, 'lock', None):
     setattr(kombu.utils.cached_property, 'lock', functools.cached_property(lambda _: RLock()))
     # Must call __set_name__ here since this cached property is not defined in the context of a class
     # Refer to https://docs.python.org/3/reference/datamodel.html#object.__set_name__
     kombu.utils.cached_property.lock.__set_name__(kombu.utils.cached_property, 'lock')
 
-#def celery_init_app(app):
+
+# def celery_init_app(app):
 #    celery = Celery(
 #        app.import_name,
 #        broker=app.config['broker_url']
