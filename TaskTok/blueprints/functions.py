605d0c8d0df2243584aa7f9067c299e1977bb5cf
John Aciares <49409536+jaciares@users.noreply.github.com>
2023-11-24 19:57:57 -0500

    cleaned up views and functions.py in PEP8

--- TaskTok/blueprints/functions.py
+++ TaskTok/blueprints/functions.py
@@ -1,21 +1,22 @@
 import subprocess
 import socket
 
-#Check to see if celery workers are running. If not, we'll want to display this somewhere as the 'Email Notification System is not running'
-def verifyCeleryWorker():
+
+# Check to see if celery workers are running. If not, we'll want to display this somewhere as the 'Email Notification
+# System is not running'
+def verify_celery_worker():
     try:
-        result = subprocess.check_output(['celery','-A', 'RemindMeClient.Client.celery', 'status'])
+        result = subprocess.check_output(['celery', '-A', 'RemindMeClient.Client.celery', 'status'])
         return True if result else False
     except subprocess.CalledProcessError as error:
         return False
-    
 
 
-def verifyMessageBrokerOnline(host, port, timeout):
+def verify_message_broker_online(host, port, timeout):
     try:
         # Create a socket object
         s = socket.create_connection((host, port), timeout=timeout)
-        
+
         # If the connection was successful, close the socket and return True
         print("Successfully connected to port 5672!")
         s.close()
@@ -25,4 +26,3 @@ def verifyMessageBrokerOnline(host, port, timeout):
         print("Failed to connect to port 5672:")
         print(str(e))
         return False
-    
--- test/test_blueprint_functions.py
+++ test/test_blueprint_functions.py
@@ -4,27 +4,27 @@ import unittest
 from unittest.mock import patch, Mock
 import subprocess
 import socket
-from TaskTok.blueprints.functions import verifyCeleryWorker, verifyMessageBrokerOnline  # Replace with actual import statement
+from TaskTok.blueprints.functions import verify_celery_worker, verify_message_broker_online  # Replace with actual import statement
 
 class TestVerifyCeleryWorker(unittest.TestCase):
     #U-10.1.1
     @patch('subprocess.check_output')
     def test_verify_celery_worker_running(self, mock_check_output):
         mock_check_output.return_value = b'celery worker is running'
-        self.assertTrue(verifyCeleryWorker())
+        self.assertTrue(verify_celery_worker())
 
     # U-10.1.2
     @patch('subprocess.check_output')
     def test_celery_worker_invalid(self, mock_check_output):
         mock_check_output.return_value = b''
-        result = verifyCeleryWorker()
+        result = verify_celery_worker()
         self.assertFalse(result)
 
     # U-10.1.3
     @patch('subprocess.check_output')
     def test_verify_celery_worker_not_running(self, mock_check_output):
         mock_check_output.side_effect = subprocess.CalledProcessError(1, 'cmd', "error")
-        self.assertFalse(verifyCeleryWorker())
+        self.assertFalse(verify_celery_worker())
 
 class TestVerifyMessageBrokerOnline(unittest.TestCase):
     # U-10.2.1
@@ -32,14 +32,14 @@ class TestVerifyMessageBrokerOnline(unittest.TestCase):
     def test_verify_message_broker_online(self, mock_create_connection):
         mock_socket = Mock()   
         mock_create_connection.return_value = mock_socket
-        self.assertTrue(verifyMessageBrokerOnline('localhost', 5672, 5))
+        self.assertTrue(verify_message_broker_online('localhost', 5672, 5))
         mock_socket.close.assert_called_once_with()
 
     # U-10.2.2
     @patch('socket.create_connection')
     def test_verify_message_broker_offline(self, mock_create_connection):
         mock_create_connection.side_effect = socket.error()
-        self.assertFalse(verifyMessageBrokerOnline('localhost', 5672, 5))
+        self.assertFalse(verify_message_broker_online('localhost', 5672, 5))
 
 if __name__ == '__main__':
     unittest.main()
