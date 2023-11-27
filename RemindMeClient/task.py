d92f2b6d987c3cea38647843534174f04dbd93d4
beatles1235 <beatles1235@gmail.com>
2023-11-27 12:29:41 -0500

    adding task for checking tasks due

--- RemindMeClient/task.py
+++ RemindMeClient/task.py
@@ -5,14 +5,14 @@ from flask_mail import Message
 
 
 
-
-
-
-
 @shared_task(bind=True)
 def send_email(self, email_to, subject, body):
     print('hello')
-    #msg = Message(subject, recipients=[email_to])
-    ## msg.body=body
-    #msg.html = body
-    #flaskMail.send(msg)
+    msg = Message(subject, recipients=[email_to])
+    msg.body=body
+    msg.html = body
+    flaskMail.send(msg)
+
+@celery_worker.task
+def check_tasks_ready():
+    print('this will use celery beat to check tasks')
