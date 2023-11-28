086f56238154a9dda77cc6a6f7c768a9dc10912e
beatles1235 <beatles1235@gmail.com>
2023-11-27 19:35:19 -0500

    Update tasks.py

--- RemindMeClient/tasks.py
+++ RemindMeClient/tasks.py
@@ -1,22 +1,29 @@
 
 from TaskTok.extensions import flaskMail, celery_worker
 from celery import shared_task
+from celery.utils.log import get_task_logger
 from flask_mail import Message
 
-
+logger = get_task_logger(__name__)
 @celery_worker.on_after_configure.connect
 def setup_beat_tasks(sender, **kwargs):
     print('************************** SETTING UP PERIODIC TASKS **************************************')
-    sender.add_periodic_task(10.0, check_tasks_ready.s(), name="Analyze tasks")
+    sender.add_periodic_task(60.0, check_tasks_ready.s(), name="Analyze tasks")
 
 @shared_task(bind=True)
 def send_email(self, email_to, subject, body):
-    print('hello')
+    logger.info("Starting send_email task")
+    logger.info('email_to: [%s], subject: [%s, body: [%s]', email_to, subject, body)
     msg = Message(subject, recipients=[email_to])
     msg.body=body
     msg.html = body
-    flaskMail.send(msg)
+    try:
+        flaskMail.send(msg)
+        logger.info("send_email was successful!")
+    except Exception as e:
+        logger.info("send_email failed! error: %s", e)
 
 @celery_worker.task
 def check_tasks_ready():
     print('this will use celery beat to check tasks')
+    send_email.delay('jason.supple.27@gmail.com', "Periodic Email Test", 'Testing periodic tasks')
