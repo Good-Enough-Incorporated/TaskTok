dcb10ce801244650b19b636cd4075e32bad581ae
beatles1235 <beatles1235@gmail.com>
2023-11-27 19:06:31 -0500

    updated to tasks

--- TaskTok/utilities.py
+++ TaskTok/utilities.py
@@ -1,8 +1,8 @@
-import RemindMeClient.task 
+import RemindMeClient.tasks 
 
 def email_message(email_to, subject, body):
-    RemindMeClient.task.send_email.delay(email_to, subject, body)
+    RemindMeClient.tasks.send_email.delay(email_to, subject, body)
 
 def check_emails_overdue():
     print('testing function')
-    RemindMeClient.task.check_tasks_ready.delay()
+    RemindMeClient.tasks.check_tasks_ready.delay()
