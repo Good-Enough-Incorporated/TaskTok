6f3979e43fd8b1289a4e5eea1d042ef2a191a696
beatles1235 <beatles1235@gmail.com>
2023-11-30 19:24:10 -0500

    to support email notifications

--- RemindMeClient/tasks.py
+++ RemindMeClient/tasks.py
@@ -4,8 +4,9 @@ from celery.utils.log import get_task_logger
 from flask_mail import Message
 from datetime import datetime, timedelta
 from TaskTok.extensions import flaskMail, celery_worker
-from TaskTok.models import TaskReminder
+from TaskTok.models import TaskReminder, User
 from TaskTok.extensions import db
+from flask import render_template
 logger = get_task_logger(__name__)
 @celery_worker.on_after_configure.connect
 def setup_beat_tasks(sender, **kwargs):
@@ -28,10 +29,29 @@ def send_email(self, email_to, subject, body):
     except Exception as e:
         logger.info("send_email failed! error: %s", e)
 
+@shared_task(bind=True)
+def send_task_reminder(self, email_to, subject, message):
+    logger.info("Starting send_task_reminder task")
+    msg = Message(subject, recipients=[email_to])
+    msg.html = message
+    try:
+        flaskMail.send(msg)
+        logger.info("send_email was successful!")
+    except Exception as e:
+        logger.info("send_email failed! error: %s", e)
+
 @celery_worker.task
 def check_tasks_ready():
     print('this will use celery beat to check tasks')
     current_time = datetime.now()
-    task_list = TaskReminder.query.filter(TaskReminder.task_dueDate >= (current_time-(timedelta(days=30)))).all()
+    #task_list = TaskReminder.query.filter(TaskReminder.task_dueDate >= (current_time-(timedelta(days=30)))).all()
+    task_list = TaskReminder.query.filter(TaskReminder.task_dueDate >= (current_time-(timedelta(days=0)))).all()
     logger.info('There are %s tasks ready for email alerts!', len(task_list))
+    for task in task_list:
+        # we need to construct the email for each task, and queue up our emails
+        logger.info(f'Working on {task.id}')
+        email_message = render_template('email/taskEmailTemplate', username = task.owner_username, task_name=task.task_name, due_date=task.dueDate, duedate_offset=task.reminderOffSetTime, message=task.task_message)
+        user = User.query.filter_by(username=task.owner_username)
+        subject = f'TaskTok - Reminder for {task.task_name}'
+        send_task_reminder.delay(user.email, subject, email_message)
     #send_email.delay('jason.supple.27@gmail.com', "Periodic Email Test", 'Testing periodic tasks')
