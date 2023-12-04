8478b8d5291b9d0efd4356f8d09624d60cc446c7
beatles1235 <beatles1235@gmail.com>
2023-12-03 21:23:27 -0500

    fix celery (bandaid...)

--- TaskTok/blueprints/views.py
+++ TaskTok/blueprints/views.py
@@ -8,7 +8,7 @@ from pytz import all_timezones
 from flask import Blueprint
 from flask import render_template, url_for, request, redirect
 from flask_jwt_extended import jwt_required, current_user, get_csrf_token
-from TaskTok.forms import LoginForm, UpdateSettingsForm, AddTaskForm
+from TaskTok.forms import LoginForm, UpdatePersonalInfoForm, UpdateCredentialsForm, AddTaskForm
 from TaskTok.extensions import generate_links
 
 from TaskTok.extensions import db
@@ -78,20 +78,27 @@ def userSettings():
         
     access_token_cookie = request.cookies.get('access_token_cookie')
     token = get_csrf_token(access_token_cookie)
-    form = UpdateSettingsForm()
+    personal_info_form = UpdatePersonalInfoForm()
+    credential_form    = UpdateCredentialsForm()
     error = None
     side_nav_menu_items = generate_links()
     
+    if request.method == 'GET':
+        personal_info_form.username.data = personal_information['username']
+        personal_info_form.email.data = personal_information['email']
+        personal_info_form.first_name.data = personal_information['first_name']
+        personal_info_form.last_name.data = personal_information['last_name']
       
     if request.method == 'POST':
         print("POST")
         
-        if form.validate_on_submit():
-            print('validating')
-            submit_type = request.args.get('form_id')
-            #using the form_id, we can check which portions to update
-            if submit_type == 'update_information':
-
+        
+        print('validating')
+        submit_type = request.args.get('form_id')
+        #using the form_id, we can check which portions to update
+        if submit_type == 'update_information':
+            if personal_info_form.validate_on_submit():
+                print('[/userSettings] VALIDATED INPUT')
                 username = request.form.get('username')
                 email = request.form.get('email')
                 first_name = request.form.get('first_name')
@@ -104,8 +111,22 @@ def userSettings():
                 
                 db.session.commit()
                 return redirect(url_for('views.userSettings'))
-            if submit_type == 'update_credentials':
-
+        else:
+            if personal_info_form.username.errors:
+                error = personal_info_form.username.errors[0]
+                print('username' + error)
+            if personal_info_form.email.errors:
+                error = personal_info_form.email.errors[0]
+                print('email' + error)
+            if personal_info_form.first_name.errors:
+                error = personal_info_form.first_name.errors[0]
+                print('first_name' + error)
+            if personal_info_form.last_name.errors:
+                error = personal_info_form.last_name.errors[0]
+                print('last_name' + error)
+        if submit_type == 'update_credentials':
+            if credential_form.validate_on_submit():
+                print('[/userSettings] update_information form pressed')
                 user = User.get_user_by_id(user_id=current_user.id)
 
                 current_password = request.form.get('current_password')
@@ -114,10 +135,22 @@ def userSettings():
                     #update their password
                     print('updating password')
                     user.set_password(new_password)
+                    db.session.commit()
+            else:
+                if credential_form.current_password.errors:
+                    error = credential_form.current_password.errors[0]
+                    print('current_password' + error)
+                if credential_form.new_password.errors:
+                    error = credential_form.new_password.errors[0]
+                    print(error)
+                if credential_form.new_password_confirm.errors:
+                    error = credential_form.new_password_confirm.errors[0]
+                    print(error)
+
         
     else:
         print("GET")
     #compare if changed
     #if changed update
-    return render_template('profile.html', username=personal_information['username'],first_name=personal_information['first_name'],  last_name=personal_information['last_name'], email=personal_information['email'], form=form, csrf_token=token, sideNavMenuItems=side_nav_menu_items, timezones = all_timezones)
+    return render_template('profile.html', username=personal_information['username'],first_name=personal_information['first_name'],  last_name=personal_information['last_name'], email=personal_information['email'], personal_info_form=personal_info_form, credential_form=credential_form, csrf_token=token, sideNavMenuItems=side_nav_menu_items, timezones = all_timezones)
 
