a439bb76682ddc72865930df5957ee3216be5ce2
John Aciares <49409536+jaciares@users.noreply.github.com>
2023-12-06 20:04:36 -0500

    got new bar working and setup for settings(profile.html)

--- TaskTok/blueprints/views.py
+++ TaskTok/blueprints/views.py
@@ -14,6 +14,7 @@ from werkzeug.datastructures import MultiDict
 
 from TaskTok.extensions import db
 from TaskTok.models import User
+
 #  ----------- Unused imports: Needs review --------------
 #  from TaskTok.Server import create_app
 # from flask import current_app
@@ -21,7 +22,6 @@ from TaskTok.models import User
 #  import requests
 
 views = Blueprint('views', __name__)
-
 
 
 # our root route (home)
@@ -51,24 +51,24 @@ def main_page():
 def home():
     # check for JWT in cookie
     form = AddTaskForm()
-    side_nav_menu_items = generate_links()
+    nav_menu_items = generate_links()
     cookies = {'access_token_cookie': request.cookies.get('access_token_cookie')}
     # response = requests.get(url_for('auth.getCurrentUser',_external=True), cookies=cookies)
 
-    return render_template('home.html', username=current_user.username, sideNavMenuItems=side_nav_menu_items, form=form)
+    return render_template('home.html', username=current_user.username, navMenuItems=nav_menu_items, form=form)
+
 
 @views.route('/admin')
 def admin():
-    side_nav_menu_items = generate_links()
-    return render_template('admin.html', sideNavMenuItems=side_nav_menu_items)
+    nav_menu_items = generate_links()
+    return render_template('admin.html', navMenuItems=nav_menu_items)
 
 
 @views.route('/userSettings', methods=['GET', "POST"])
 @jwt_required()
 def userSettings():
-    
     personal_information = {
-        'username' : current_user.username,
+        'username': current_user.username,
         'email': current_user.email,
         'first_name': current_user.first_name,
         'last_name': current_user.last_name
@@ -77,25 +77,25 @@ def userSettings():
         print(value)
         if value is None:
             personal_information[key] = ''
-        
+
     access_token_cookie = request.cookies.get('access_token_cookie')
     token = get_csrf_token(access_token_cookie)
     personal_info_form = UpdatePersonalInfoForm()
-    credential_form    = UpdateCredentialsForm()
+    credential_form = UpdateCredentialsForm()
     timezone_form = UpdateTimeZoneForm()
 
     if request.method == 'GET':
-    #after redirect errors are lost
-    #save them in session, and restore on next get method
+        # after redirect errors are lost
+        # save them in session, and restore on next get method
         if 'credential_form' in session:
             credential_form_data = session.pop('credential_form')
-            ##need MutliDict otherwise flask_wtf assumes its prepopulated
-            #data
+            # TODO: need MutliDict otherwise flask_wtf assumes its prepopulated
+            # data
             formData = MultiDict(credential_form_data)
             credential_form = UpdateCredentialsForm(formdata=formData)
-            #generate the errors again
+            # generate the errors again
             credential_form.validate()
-            
+
         elif 'personal_info_form' in session:
             personal_info_form = UpdatePersonalInfoForm(formdata=MultiDict(session.pop('personal_info_form')))
             personal_info_form.validate()
@@ -103,11 +103,11 @@ def userSettings():
             timezone_form = UpdateTimeZoneForm(formdata=MultiDict(session.pop('timezone_form')))
             timezone_form.validate()
     error = None
-    side_nav_menu_items = generate_links()
+    nav_menu_items = generate_links()
 
     if request.method == 'POST':
         submit_type = request.args.get('form_id')
-        #using the form_id, we can check which portions to update
+        # using the form_id, we can check which portions to update
         if submit_type == 'update_information':
             if personal_info_form.validate():
                 # TODO: shall we let the user update these?
@@ -121,12 +121,12 @@ def userSettings():
 
                 user.first_name = first_name
                 user.last_name = last_name
-                
+
                 db.session.commit()
                 flash("Personal information was updated!", 'personal')
                 return redirect(url_for('views.userSettings'))
             else:
-                
+
                 if personal_info_form.username.errors:
                     error = personal_info_form.username.errors[0]
                     print('username' + error)
@@ -153,14 +153,14 @@ def userSettings():
                 current_password = request.form.get('current_password')
                 new_password = request.form.get('new_password')
                 if user.verify_password(current_password):
-                    #update their password
+                    # update their password
                     print('updating password')
                     user.set_password(new_password)
                     db.session.commit()
                     flash("Password was updated!", 'credential')
                     return redirect(url_for('views.userSettings'))
                 else:
-                    flash ("Password was incorrect, please try again", 'credential-error')
+                    flash("Password was incorrect, please try again", 'credential-error')
                     return redirect(url_for('views.userSettings'))
 
 
@@ -177,9 +177,6 @@ def userSettings():
                 session['credential_form'] = credential_form.data
                 return redirect(url_for('views.userSettings'))
 
-                    
-
-
         if submit_type == 'update_timezone':
             if timezone_form.validate():
                 user = User.get_user_by_id(user_id=current_user.id)
@@ -192,16 +189,17 @@ def userSettings():
             else:
                 session['timezone_form'] = timezone_form.data
             return redirect(url_for('views.userSettings'))
-   
-    #grab the values from our User modal to prepopulate some of the filled out information
+
+    # grab the values from our User modal to prepopulate some of the filled out information
     personal_info_form.username.data = personal_information['username']
     personal_info_form.email.data = personal_information['email']
     personal_info_form.first_name.data = personal_information['first_name']
     personal_info_form.last_name.data = personal_information['last_name']
     timezone_form.timezone_name.data = current_user.timezone
     timezone_form.daylight_savings = current_user.daylight_savings
-    
 
-   
-    return render_template('profile.html', username=personal_information['username'],first_name=personal_information['first_name'],  last_name=personal_information['last_name'], email=personal_information['email'], personal_info_form=personal_info_form, credential_form=credential_form, timezone_form=timezone_form, csrf_token=token, sideNavMenuItems=side_nav_menu_items, timezones = all_timezones)
-
+    return render_template('profile.html', username=personal_information['username'],
+                           first_name=personal_information['first_name'], last_name=personal_information['last_name'],
+                           email=personal_information['email'], personal_info_form=personal_info_form,
+                           credential_form=credential_form, timezone_form=timezone_form, csrf_token=token,
+                           navMenuItems=nav_menu_items, timezones=all_timezones)
