f1c063b6221a7e671f8dafc4d5ab3afc6a853442
beatles1235 <beatles1235@gmail.com>
2023-12-11 21:26:10 -0500

    Update functions.py

--- TaskTok/functions.py
+++ TaskTok/functions.py
@@ -1,8 +1,15 @@
+"""
+This module provides utility functions for various system checks and operations
+related to Celery workers, message brokers, and email token generation and verification.
+It includes functions to verify the operational status of Celery workers and message brokers,
+and to manage secure tokens for email-related processes using the itsdangerous library.
+"""
+import os
 import subprocess
 import socket
 from uuid import uuid4
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
-import os
+
 
 
 #  ----------- Unused Imports: Needs review ---------------
@@ -12,6 +19,13 @@ import os
 # Check to see if celery workers are running. If not, we'll want to display this somewhere as the 'Email Notification
 # System is not running'
 def verify_celery_worker():
+    """
+    Checks if the Celery worker is running by executing a subprocess command.
+    This function is useful for verifying the operational status of Celery
+    workers in the system.
+
+    :return: True if the Celery worker is running, False otherwise.
+    """
     try:
         result = subprocess.check_output(['celery', '-A', 'RemindMeClient.Client.celery_worker', 'status'])
         # print(f"OUTPUT RESULT: {result}")
@@ -23,6 +37,16 @@ def verify_celery_worker():
 
 
 def verify_message_broker_online(host, port, timeout):
+    """
+    Attempts to establish a socket connection to the message broker to verify its
+    operational status. This function is particularly useful for checking the
+    connectivity with the message broker service.
+
+    :param host: The hostname of the message broker.
+    :param port: The port on which the message broker is listening.
+    :param timeout: The timeout period for the connection attempt.
+    :return: True if the connection to the message broker is successful, False otherwise.
+    """
     try:
         # Create a socket object
         s = socket.create_connection((host, port), timeout=timeout)
@@ -39,6 +63,14 @@ def verify_message_broker_online(host, port, timeout):
 
 
 def generate_email_token(email):
+    """
+    Generates a secure token for email verification purposes. This function
+    uses the itsdangerous URLSafeTimedSerializer to create a
+    time-limited token, which includes the email and a unique identifier.
+
+    :param email: The email address to be included in the token.
+    :return: A securely generated token.
+    """
     timed_serializer = URLSafeTimedSerializer(os.environ.get('EMAIL_VERIFICATION_SECRET'))
     token_id = str(uuid4())
     token_data = {"email":email,"jti":token_id}
@@ -46,6 +78,14 @@ def generate_email_token(email):
 
 
 def verify_email_token(token_data, expiration=1800):
+    """
+    Verifies a given token for email operations. It checks the token's
+    validity and ensures it hasn't expired, using the itsdangerous URLSafeTimedSerializer.
+
+    :param token_data: The token data to verify.
+    :param expiration: The expiration time in seconds for the token (default is 1800 seconds).
+    :return: The token data if verification is successful, False if the token is expired or invalid.
+    """
     timed_serializer = URLSafeTimedSerializer(os.environ.get('EMAIL_VERIFICATION_SECRET'))
     try:
         token_data = timed_serializer.loads(token_data, salt=os.environ.get('SECURITY_PASSWORD_SALT'), max_age=expiration)
